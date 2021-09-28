c617f7657ce24a3a74204c3b9e7d1c746575ad07
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-09-28 09:20:21 +0530

    Add files via upload

---
+++ EV/charging point/NTPC.py
@@ -0,0 +1,55 @@
+# To start pyspark shell
+# ./pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2
+
+# To Create output topic
+# ./kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ntpc1
+
+# To run using spark submit
+# ./spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 home/ubuntu/Desktop/EV/Charging_Point/NTPC.py
+
+# To get data from terminal using kafka consumer
+# ./kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic ntpc1 --from-beginning
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+spark = SparkSession \
+    .builder \
+    .appName("Charging_companies") \
+    .getOrCreate()
+
+raw_df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "ntpc") \
+    .option("startingOffsets", "latest") \
+    .load() \
+    .selectExpr("CAST(value AS STRING)")
+
+# Created Schema for Structured Streaming
+
+#timestamp,open,high,low,close,volume
+
+schema = StructType(
+    [
+            StructField("open", StringType()),
+            StructField("high", StringType()),
+            StructField("low", StringType()),
+            StructField("close", StringType()),
+            StructField("volume", StringType()),
+            StructField("date", StringType())
+    ])
+# Applied schema on data
+data1= raw_df.select(from_json(raw_df.value, schema).alias("data"))
+
+#fetching required data for processing usecases
+output_df1 = data1.select(to_json(struct(col("data.volume"),col("data.date"))).alias("value"))
+
+# Sending the data to kafka brocker
+query = output_df1.writeStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("checkpointLocation", "/tmp/checkpoint2").option("topic", "ntpc1").start()
+
+# Waits for the termination signal from user
+query.awaitTermination()
